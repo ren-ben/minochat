b304006dc1ab1a9a4f54623a04b127f618d7694d
ben <OliGaming122@gmail.com>
2023-05-13 08:50:53 +0200

    add firebase
    integrate firebase with mino

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mino/colors.dart';
+import 'package:mino/firebase_options.dart';
 import 'package:mino/resp/responsive_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
